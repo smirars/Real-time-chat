66cbf4ca62969e3555829c947c7d7c9d87d2adef
Arsonist <adsmi888@gmail.com>
2024-12-02 21:50:20 +0300

    add login component

--- chat/src/components/Login.js
+++ chat/src/components/Login.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, {useContext} from 'react';
+import {Button, Container, Grid} from "@material-ui/core";
+import Box from "@material-ui/core/Box";
+import {Context} from "../index";
+import firebase from "firebase";
+
 
 const Login = () => {
-    return (
-        <div>
-            Login
-        </div>
-    )
-}
+    const {auth} = useContext(Context)
 
-export default Login
+    const login = async () => {
+        const provider = new firebase.auth.GoogleAuthProvider()
+        const {user} = await auth.signInWithPopup(provider)
+        console.log(user)
+    }
+
+    return (
+        <Container>
+            <Grid container
+                  style={{height: window.innerHeight - 50}}
+                  alignItems={"center"}
+                  justify={"center"}
+            >
+                <Grid style={{width:400, background: 'lightgray'}}
+                      container
+                      alignItems={"center"}
+                      direction={"column"}
+                >
+                    <Box p={5}>
+                        <Button onClick={login} variant={"outlined"}>Войти с помощью Google</Button>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Container>
+    );
+};
+
+export default Login;
