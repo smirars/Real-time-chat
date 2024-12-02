84315514dd99f6eb18e498fe533043bed28cff84
Arsonist <adsmi888@gmail.com>
2024-12-02 22:21:26 +0300

    fix for modular approach

--- chat/src/components/AppRouter.js
+++ chat/src/components/AppRouter.js
@@ -1,32 +1,28 @@
-import React, {useContext} from 'react';
-import {Route, Switch, Redirect} from 'react-router-dom'
-import {privateRoutes, publicRoutes} from "../routes";
-import {CHAT_ROUTE, LOGIN_ROUTE} from "../utils/consts";
-import {useAuthState} from "react-firebase-hooks/auth";
-import {Context} from "../index";
+import React, { useContext } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import { privateRoutes, publicRoutes } from "../routes";
+import { ROUTES } from "../utils/constants";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Context } from "../index";
 
 const AppRouter = () => {
-    const {auth} = useContext(Context)
-    const [user] = useAuthState(auth)
+    const { auth } = useContext(Context);
+    const [user] = useAuthState(auth);
 
-    return user ?
-        (
-            <Switch>
-                {privateRoutes.map(({path, Component}) =>
-                    <Route key={path} path={path} component={Component} exact={true}/>
-                )}
-                <Redirect to={CHAT_ROUTE}/>
-            </Switch>
-        )
-        :
-        (
-            <Switch>
-                {publicRoutes.map(({path, Component}) =>
-                    <Route key={path} path={path} component={Component} exact={true}/>
-                )}
-                <Redirect to={LOGIN_ROUTE}/>
-            </Switch>
-        )
+    return (
+        <Switch>
+            {user ? (
+                privateRoutes.map(({ path, Component }) => (
+                    <Route key={path} path={path} component={Component} exact />
+                ))
+            ) : (
+                publicRoutes.map(({ path, Component }) => (
+                    <Route key={path} path={path} component={Component} exact />
+                ))
+            )}
+            <Redirect to={user ? ROUTES.CHAT : ROUTES.LOGIN} />
+        </Switch>
+    );
 };
 
 export default AppRouter;
