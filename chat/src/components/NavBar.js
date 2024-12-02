1ffc2b602585cbbc72b6cc36a9b338b2b669a812
Arsonist <adsmi888@gmail.com>
2024-12-02 21:51:50 +0300

    add navbar component

--- chat/src/components/NavBar.js
+++ chat/src/components/NavBar.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, {useContext} from 'react';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import {Button, Grid} from "@material-ui/core";
+import {NavLink} from "react-router-dom";
+import {LOGIN_ROUTE} from "../utils/consts";
+import {Context} from "../index";
+import {useAuthState} from "react-firebase-hooks/auth";
 
 const Navbar = () => {
-    return (
-        <div>
-            Navbar
-        </div>
-    )
-}
+    const {auth} = useContext(Context)
+    const [user] = useAuthState(auth)
 
-export default Navbar
+    return (
+        <AppBar color={"secondary"} position="static">
+            <Toolbar variant={"dense"}>
+                <Grid container justify={"flex-end"}>
+                    {user ?
+                        <Button onClick={() => auth.signOut()} variant={"outlined"}>Выйти</Button>
+                        :
+                        <NavLink to={LOGIN_ROUTE}>
+                            <Button variant={"outlined"}>Логин</Button>
+                        </NavLink>
+                    }
+                </Grid>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default Navbar;
