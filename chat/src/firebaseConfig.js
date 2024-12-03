1bc49873461d5f7c11d796afab3c43e6cd59ad77
Arsonist <adsmi888@gmail.com>
2024-12-03 19:17:14 +0300

    change config

--- chat/src/firebaseConfig.js
+++ chat/src/firebaseConfig.js
@@ -3,14 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "Ваш API-ключ",
-    authDomain: "Ваш домен авторизации",
-    projectId: "Ваш ID проекта",
-    storageBucket: "Ваш storage bucket",
-    messagingSenderId: "Ваш ID отправки сообщений",
-    appId: "Ваш App ID",
-    measurementId: "Ваш Measurement ID"
-};
+    apiKey: "AIzaSyBcqA4IBMZmdJ0WaMA3f5PVu6TEDzMGhTE",
+    authDomain: "realtime-chat0.firebaseapp.com",
+    projectId: "realtime-chat0",
+    storageBucket: "realtime-chat0.firebasestorage.app",
+    messagingSenderId: "316751006462",
+    appId: "1:316751006462:web:6f6221c76a8e2f334873af",
+    measurementId: "G-YWHGHHSJFH"
+  };
 
 const app = initializeApp(firebaseConfig);
 
