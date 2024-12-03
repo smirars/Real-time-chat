a8e0d94616d39f2c42e7eed524035aa3a8d961c2
Arsonist <adsmi888@gmail.com>
2024-12-03 19:26:09 +0300

    add redirect method in case error with popup

--- chat/src/components/Login.js
+++ chat/src/components/Login.js
@@ -1,19 +1,28 @@
 import React, { useContext } from 'react';
 import { Button, Container, Grid, Box } from "@mui/material";
 import { Context } from "../index";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, signInWithRedirect } from "firebase/auth";
 
 const Login = () => {
     const { auth } = useContext(Context);
 
     const login = async () => {
         const provider = new GoogleAuthProvider();
-
+    
         try {
             const result = await signInWithPopup(auth, provider);
             console.log(result.user);
         } catch (error) {
-            console.error("Ошибка входа:", error);
+            if (error.code === 'auth/popup-blocked') {
+                console.warn('Всплывающее окно заблокировано, перенаправление...');
+                try {
+                    await signInWithRedirect(auth, provider);
+                } catch (redirectError) {
+                    console.error("Ошибка при перенаправлении:", redirectError);
+                }
+            } else {
+                console.error("Ошибка входа:", error);
+            }
         }
     };
 
