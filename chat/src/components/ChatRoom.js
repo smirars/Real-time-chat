ff421bb1770f2cf0b185e71812b6dd0d6db203ba
Arsonist <adsmi888@gmail.com>
2024-12-03 20:33:03 +0300

    fixed chat

--- chat/src/components/ChatRoom.js
+++ chat/src/components/ChatRoom.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import { firestore } from '../firebaseConfig'; 
 import { collection, addDoc, query, onSnapshot, orderBy } from 'firebase/firestore';
